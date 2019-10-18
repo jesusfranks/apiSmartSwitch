3a12b72680bd214eeebd5aa51b7519d51d18d5ef
Francisco Garcia <jesusfranks6@gmail.com>
2019-10-18 16:04:55 -0500

    env cambios

--- config/database.js
+++ config/database.js
@@ -16,7 +16,7 @@ module.exports = {
   | interacting with SQL databases.
   |
   */
-  connection: Env.get('DB_CONNECTION', 'sqlite'),
+  connection: Env.get('DB_CONNECTION', 'mysql'),
 
   /*
   |--------------------------------------------------------------------------
@@ -51,15 +51,14 @@ module.exports = {
   mysql: {
     client: 'mysql',
     connection: {
-      host: Env.get('DB_HOST', 'localhost'),
-      port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', 'root'),
-      password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
+      host: Env.get('DB_HOST', 'remotemysql.com'),
+      port: Env.get('DB_PORT', '3306'),
+      user: Env.get('DB_USER', 'lfx0jSpkN6'),
+      password: Env.get('DB_PASSWORD', '7EhF19QDAe'),
+      database: Env.get('DB_DATABASE', 'lfx0jSpkN6')
     },
     debug: Env.get('DB_DEBUG', false)
   },
-
   /*
   |--------------------------------------------------------------------------
   | PostgreSQL
